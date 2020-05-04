0fda34f7a6735edec474ac690770042bb99a541c
Akshay Makkar <akshaym1103@gmail.com>
2020-05-04 06:03:07 +0530

    Form type changed

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var express = require ('express');
 var path = require ('path');
 var cookieParser = require ('cookie-parser');
 var logger = require ('morgan');
-var bodyParser = require('body-parser');
-var multer = require('multer');
 require ('dotenv').config ();
 
 var indexRouter = require ('./routes/index.route');
@@ -38,8 +36,6 @@ app.use (express.json ());
 app.use (express.urlencoded ({extended: false}));
 app.use (cookieParser ());
 app.use (express.static (path.join (__dirname, 'public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.use ('/', indexRouter);
 app.use ('/users', usersRouter);
