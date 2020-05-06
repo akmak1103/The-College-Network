a891910a7e7a576c73bee56cdc7cc3c4ddb8cc3d
Akshay Makkar <akshaym1103@gmail.com>
2020-05-06 22:40:42 +0530

    Fix Verify email via EC2 instance

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -78,7 +78,7 @@ function sendEmail (hash, email, type) {
   });
   var mailOptions;
   if (type == 0) {
-    let link = 'http://18.222.220.88:3000/users/verify/' + hash;
+    let link = 'http://13.233.9.37/:3000/users/verify/' + hash;
     mailOptions = {
       to: email,
       subject: '[The College Network] Verify your Email Address',
